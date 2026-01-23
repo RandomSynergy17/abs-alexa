2ea253e96a2317b346b12985b6a4aa9f114b7b57
RandomSynergy17 <Github@randomsynergy.com>
2026-01-24 01:05:33 +0400

    config change

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlJZCI6ImY4Mzk1MDlhLTYwNjQtNDI0OC04NzRiLWM2NDY1MWJjNzEzNSIsIm5hbWUiOiJBbGV4YSIsInR5cGUiOiJhcGkiLCJpYXQiOjE3NjkyMDIyMzB9.03vfpzWJtSVdqjXwRPhNuTWYP8y0Tr1NQhotsMaH9VI',
+    SERVER_URL: 'https://audioshelf.randomsynergy.xyz/',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
     // CFAccessClientId: 'xxxxxxxxx.access',
